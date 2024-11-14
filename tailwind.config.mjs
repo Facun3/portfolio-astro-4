e9aa94cdae69ea817077d5f3ae81bbad10b74ccc
Facundo Choque <facusilberman@gmail.com>
2024-11-14 00:57:17 -0300

    Added containers to sections, changed hero phrase

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,9 +8,14 @@ export default {
 				'0%': { transform: 'translateX(0)' },
 				'100%': { transform: 'translateX(-100%)' },
 			  },
+			  fadeIn: {
+				'0%': { opacity: '0' },
+				'100%': { opacity: '1' },
+			  }
 			},
 			animation: {
 			  scroll: 'scroll 20s linear infinite',
+			  fadeIn: 'fadeIn 1s ease-out forwards',
 			},
 		  },
 	},
