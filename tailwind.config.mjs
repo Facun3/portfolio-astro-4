61d40ef8b8e4b949f57e6aa1b8b8ee657dc9d31a
Facundo Choque <facusilberman@gmail.com>
2024-11-13 23:24:57 -0300

    New seccion WhatIDo added, style fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+			  scroll: {
+				'0%': { transform: 'translateX(0)' },
+				'100%': { transform: 'translateX(-100%)' },
+			  },
+			},
+			animation: {
+			  scroll: 'scroll 20s linear infinite',
+			},
+		  },
 	},
 	plugins: [],
 	darkMode: 'class',
